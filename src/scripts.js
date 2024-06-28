379bb07b5450dc46e4558208fb33368e881b4135
putradevl <saputraluhur27@gmail.com>
2024-06-28 21:40:53 +0700

    tombol

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,17 @@
 //Navbar Fixed
 window.onscroll = function() {
     const header = document.querySelector('header');
-    const fixedNav = header.offsetTop; 
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top') 
 
     if(window.pageYOffset > fixedNav){
         header.classList.add('navbar-fixed');
-     } else {
-            header.classList.remove('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
         }
     };
 
@@ -19,4 +24,12 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 
+});
+
+// klik diluar
+window.addEventListener('click', function (e){
+    if(e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+    }
 });
